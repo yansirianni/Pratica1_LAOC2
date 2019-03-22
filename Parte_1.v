278b088102230501ec773d5b9266cbcc6d19aa66
Yan Sirianni <yanvictorsa@yahoo.com.br>
2019-03-22 14:35:13 -0300

    add mif in project

--- Parte_1.v
+++ Parte_1.v
@@ -6,9 +6,13 @@ module Parte_1 (HEX7, HEX6, HEX5, HEX4, HEX3, HEX2, HEX1, HEX0, SW, CLOCK_50);
 	wire [7:0]ram_out;
 	
 	//module decoder (input [3:0]entrada, output reg[6:0]saida);
+	
+	assign HEX2 = 7'b1111111;
+	assign HEX3 = 7'b1111111;
+	
 	decoder dec0(SW[3:0], HEX0);
 	decoder dec1({3'b0,SW[4]}, HEX1);
-	
+		
 	decoder dec2(SW[13:10], HEX4);
 	decoder dec3(SW[17:14], HEX5);
 	
