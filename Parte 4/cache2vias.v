e904abfc64350f542a982e8d0602cd6c25c4a751
Heitor Amaral <heitor.amaral.santos@gmail.com>
2019-04-09 23:29:52 -0300

    modified cache initialization

--- Parte 4/cache2vias.v
+++ Parte 4/cache2vias.v
@@ -9,8 +9,8 @@ module cache2vias(input[4:0] addr,
 						wire [11:0] data_write1, data_write2, data_out1, data_out2;
 
 						
-  via via1(addr[3:0], clock, data_write1, (wren &&  data_out1[10]), data_out1);
-  via via2(addr[3:0], clock, data_write2, (wren && ~data_out1[10]), data_out2);
+  via via1(addr[3:0], clock, data_write1, wren, data_out1);
+  via via2(addr[3:0], clock, data_write2, wren, data_out2);
 
   assign hit =   (data_out1[11] && ((data_out1[9:8] == addr[4:3]) || (data_out2[9:8] == addr[4:3])));
   assign miss = ~(data_out1[11] && ((data_out1[9:8] == addr[4:3]) || (data_out2[9:8] == addr[4:3])));
